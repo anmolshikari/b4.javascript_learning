68004b6182ade14e0614df2bd47948f1e5469571
anmolshikari <Anmolshikari11111@gmail.com>
2024-11-12 21:05:46 +1100

    learning
    if else

--- day_02/app.js
+++ day_02/app.js
@@ -1,15 +1,23 @@
-const num1 = 1005
-const num2 = 1006
- if(num1===num2) {
-    console.log("Both are euqal");
- } else if (num1 > num2) {
-    console.log("num1 and num2 are equal");
- }
 
  
 
  
-  else{
-    console.log("They are not equal")
+  
    
+ 
+ const nameofday = "saturday ";
+ 
+ if (nameofday === "saturday") {
+    console.log("today is weekend");
+    console.log("today is saturday");
  }
+ else  (nameofday=== "sunday") {
+    console.log("Today is  weekend");
+    console.log("Today is sunday");
+    }
+    
+ 
+
+
+ 
+
