25e2fd8b59076c63d1d0651fa7677e8e0be11dbb
anmolshikari <Anmolshikari11111@gmail.com>
2024-11-12 19:52:01 +1100

    practice
    simple

---
+++ day_02/app.js
@@ -0,0 +1,15 @@
+const num1 = 1005
+const num2 = 1006
+ if(num1===num2) {
+    console.log("Both are euqal");
+ } else if (num1 > num2) {
+    console.log("num1 and num2 are equal");
+ }
+
+ 
+
+ 
+  else{
+    console.log("They are not equal")
+   
+ }
