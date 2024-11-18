59a566950ab5dda5d256f08567003123014b983f
anmolshikari <Anmolshikari11111@gmail.com>
2024-11-18 20:01:41 +1100

    javascript
    arrowkey

--- day_02/app.js
+++ day_02/app.js
@@ -1,23 +1,9 @@
 
  
-
+console.log("hello world");
  
   
    
  
- const nameofday = "saturday ";
- 
- if (nameofday === "saturday") {
-    console.log("today is weekend");
-    console.log("today is saturday");
- }
- else  (nameofday=== "sunday") {
-    console.log("Today is  weekend");
-    console.log("Today is sunday");
-    }
-    
- 
-
-
  
 
---
+++ day_02/day_03/app.js
@@ -0,0 +1,3 @@
+let normalName ="anmol";
+let javaScriptName= normalName + "script";
+console.log ('your javaScript Name is' + javaScriptName);
