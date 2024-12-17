16e8a12b097382e475c69f2cd0e3424b27bf8573
anmolshikari <Anmolshikari11111@gmail.com>
2024-12-17 20:50:46 +1100

    incomplete
    need to be done

--- Day_07/Day_08/Day_09/app.js
+++ Day_07/Day_08/Day_09/app.js
@@ -101,3 +101,56 @@ console.log(`It will take ${getThere(distance)} hours to get there;`);
 //const getWalkTime = ()=> { arrow Function}
 const doubleArra = [1, 2, 3, 4].map((item) => item * 2);
 console.log(doubleArra);
+//
+
+class ToDoItem {
+  constructor(title) {
+    this.title = title;
+    this.isCompleted = false;
+  }
+}
+let toDoList = [];
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const value = document.querySelector('form input').value;
+  const toDoItem = new ToDoItem(value);
+  toDoList.unshift(toDoItem);
+  renderToDoList();
+  clearInput();
+  updateTotal();
+}
+function clearInput() {
+  document.querySelector('form input').value = '';
+  if (value) {
+  }
+}
+function updateTotal() {
+  document.querySelector('#count').innerHTML = toDoList.length;
+}
+function renderToDoList() {
+  const ul = document.querySelector('ul');
+  const liHTML = toDoList.forEach((item) => {
+    const li = `<li >${item.index} < input checked = { item.isCompleted  ? "class= 'completed' " : ''} > $ {
+    item.title } <input $ {
+    item.isCompleted && 'checked'} type="chekbox" data-index= "${index}" /> </li>`;
+    return li;
+  });
+  ul.innerHTML = liHTML.join('');
+  const checkboxArray = document.querySelector('input[type="checkbox"]');
+  checkboxArray.forEach((checkboc) =>
+    checkbox.addEventListener('click', toggleCheckbox)
+  );
+}
+function toogleCheckbox(event) {
+  let index = event.target.dataset.index;
+  toDoList = toDoList.map((todo, i) => {
+    if (i == index) {
+      todo.isCompleted = !todo.isCompleted;
+    }
+    return;
+  });
+  console.log(event.target);
+}
+document
+  .querySelector('Form button')
+  .addEventListner('click', handleFormSubmit);
